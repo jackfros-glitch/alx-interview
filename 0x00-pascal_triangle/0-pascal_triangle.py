1bb5d5979dd4e5e8cac8e9c9be5732aa810d2df2
Jackfros <hirekaanm3@gmail.com>
2022-11-03 08:54:56 +0100

    pascalTriangle solution in python

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,11 @@
+def pascal_triangle(n):
+    res = [[1]]
+    for i in range(n - 1):
+        tmp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1])+ 1):
+            row.append(tmp[j] + tmp[j+1])
+        res.append(row)
+    return res
+
+print(pascal_triangle(5))
