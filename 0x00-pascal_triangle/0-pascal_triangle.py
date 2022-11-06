fa8b2b6d62e5d1d6586c89f4ea165f06e9935546
Jackfros <hirekaanm3@gmail.com>
2022-11-06 16:10:15 +0100

    docs: Add proper documentation of the function pascal_traingle and of the module

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python3
 
+''' 
+This script allows the user to genrate a list of lists of integers representing the Pascal’s triangle of n
+n -> the number of rows the users wants to generate
+This file can also be imported as a module and contains only one function
+which is:
+ * pascal_traingle -> takes in the number of rows in the pascal's Triangle to be generated and 
+        returns a list of lists of integers representing the Pascal’s triangle 
+'''
+
+
 def pascal_triangle(n):
+    ''' takes in the number of rows in the pascal's Triangle that the user wants to generate
+
+    Parameters
+    -----------
+    n : integer
+        The number of rows to be generated
+
+    Returns 
+    ------------
+    res : list
+        a list of lists of integers representing the Pascal’s triangle  
+    '''
     res = [[1]]
     for i in range(n - 1):
         tmp = [0] + res[-1] + [0]
