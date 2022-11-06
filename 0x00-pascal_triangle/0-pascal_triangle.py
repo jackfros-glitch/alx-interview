97a5aad31d391de61e766339f521eaaec8070082
Jackfros <hirekaanm3@gmail.com>
2022-11-06 19:00:01 +0100

    Refactor: refactor code base to follow PEP8 codestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-''' 
+'''
 This script allows the user to genrate a list of lists of integers representing the Pascal’s triangle of n
 n -> the number of rows the users wants to generate
 This file can also be imported as a module and contains only one function
 which is:
- * pascal_traingle -> takes in the number of rows in the pascal's Triangle to be generated and 
-        returns a list of lists of integers representing the Pascal’s triangle 
+ * pascal_traingle -> takes in the number of rows in the pascal's Triangle to be generated and
+        returns a list of lists of integers representing the Pascal’s triangle
 '''
 
 
@@ -18,16 +18,16 @@ def pascal_triangle(n):
     n : integer
         The number of rows to be generated
 
-    Returns 
+    Returns
     ------------
     res : list
-        a list of lists of integers representing the Pascal’s triangle  
+        a list of lists of integers representing the Pascal’s triangle
     '''
     res = [[1]]
     for i in range(n - 1):
         tmp = [0] + res[-1] + [0]
         row = []
-        for j in range(len(res[-1])+ 1):
-            row.append(tmp[j] + tmp[j+1])
+        for j in range(len(res[-1]) + 1):
+            row.append(tmp[j] + tmp[j + 1])
         res.append(row)
     return res
