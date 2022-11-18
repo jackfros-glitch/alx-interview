b59d9a6d4ea2f26f9f6722d54aff615cfcdf812b
Jackfros <hirekaanm3@gmail.com>
2022-11-18 17:33:27 +0100

    docs: Add documentation about the 0-lockboxes python script and proper method specification to the canUnlockAll function

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 '''
+This script takes in a list of locked boxes with the first 
+box opened by default and determines if the other boxes can
+be unlocked by the keys by the keys available in the boxes
+
+This file can also be imported as a module and contains only one function
+which is:
+ *canUnlockAll -> takes in a list of locked boxes which contains
+    keys that may or may not be able to open all the boxes in the list 
+    the function returns True if the keys available in the boxes can
+    open all the boxes in the list of boxes and false if the avaible keys 
+    cannot 
 '''
 
 def canUnlockAll(boxes: list) -> bool:
     '''
-
+    takes in a list of boxes and determine if the keys in the boxes
+    can open all the boxes available in the list
+    
     Parameters
     ---------
     boxes : list
